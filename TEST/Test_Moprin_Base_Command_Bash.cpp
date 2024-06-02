760d943a0a1b444cb7312c716e3d3e4045c3661e
Elie Duhamel <elie.duhamel@grenoble-inp.org>
2024-06-02 18:49:48 +0200

    Debut implemmentation test

--- TEST/Test_Moprin_Base_Command_Bash.cpp
+++ TEST/Test_Moprin_Base_Command_Bash.cpp
@@ -1,10 +1,15 @@
 //
 // Created by duhamel on 01/06/24.
 //
+#include <cstdio>
+
 #include "MOPRIN_Base_Type.h"
 #include "MOPRIN_Base_Command_Bash.h"
 
-
+void help(mo_user_data data) {
+        printf("Aide:\n");
+        MOPRIN_B_CB_Run_Stop();
+}
 
 
 
@@ -17,7 +22,8 @@
 
 int main(int argc,char* argv[]) {
         MOPRIN_B_Command_Bash_Init();
-
+        MOPRIN_B_CB_Registre_Commande_string("help",help);
+        MOPRIN_B_CB_Registre_Commande_Single('h',help);
 
         MOPRIN_B_CB_Run(argc,argv,nullptr);
         MOPRIN_B_CB_Release();
