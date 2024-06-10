4791aee2088a67002621bdc33eeeb0f8777afc72
Elie Duhamel <elie.duhamel@grenoble-inp.org>
2024-06-10 17:27:48 +0200

    = implemmentation test

--- TEST/Test_Moprin_Base_Command_Bash.cpp
+++ TEST/Test_Moprin_Base_Command_Bash.cpp
@@ -11,6 +11,10 @@ void help(mo_user_data data) {
         printf("Aide:\n");
         MOPRIN_B_CB_Run_Stop();
 }
+void print(mo_user_data data) {
+        printf(MOPRIN_B_CB_Get_Argument_Command_Relative(1));
+        MOPRIN_B_CB_UP(1);
+}
 
 
 
@@ -25,6 +29,7 @@ int main(int argc,char* argv[]) {
         MOPRIN_B_Command_Bash_Init();
         MOPRIN_B_CB_Registre_Commande_string("help",help);
         MOPRIN_B_CB_Registre_Commande_Single('h',help);
+        MOPRIN_B_CB_Registre_Commande_Single('p',print);
 
         MOPRIN_B_CB_Run(argc,argv,nullptr);
         MOPRIN_B_CB_Release();
